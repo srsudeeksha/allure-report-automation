8b385d4bddc796d8f983fe7af972b5b26ee8298e
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-10-24 16:50:48 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/allure-report-automation",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
