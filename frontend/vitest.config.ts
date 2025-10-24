48b58a6a30d7e924a34f18e340d6ed56bdffe001
srudeeksha <srsudeeksha@gmail.com>
2025-10-24 17:38:50 +0530

    intial commit

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -11,15 +11,15 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: [
-      "./src/tests/setup.ts",      // your custom setup
-      "allure-vitest/setup"        // necessary for Allure
+      "./src/tests/setup.ts",      
+      "allure-vitest/setup"        
     ],
     reporters: [
-      "default",                   // standard Vitest console output
+      "default",                   
       [
         "allure-vitest/reporter", 
         {
-          resultsDir: "./allure-results",  // where Allure JSON results are stored
+          resultsDir: "./allure-results",  
         },
       ],
     ],
